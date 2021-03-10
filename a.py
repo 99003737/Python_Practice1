f6198968d725e1a27268d4f79dbb18e785b3d3b6
99003737 <jeshwanth.ega@ltts.com>
2021-03-11 05:21:52 +0530

    Initial commit

---
+++ a.py
@@ -0,0 +1,26 @@
+x = "jeshu"
+y = "is goodboy"
+
+z = x+y
+
+print(z)
+print(type(z))
+print(id(z))
+a = 7
+b = 8
+print(a**b)
+print('i\'am leaning' )
+print("""this is mullti
+ggg
+line code  """)
+print(4*'string')
+user = "jesh"
+text = input()
+print(text)
+jeshu = input("jeshu")
+age = int(input())
+print(age)
+spam = "7"
+spam = spam + "0"
+eggs = int(spam) + 3
+print(float(eggs))
